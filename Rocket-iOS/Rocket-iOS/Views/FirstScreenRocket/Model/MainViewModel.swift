58a36c0982c37a469f70232fd8afb78d306c851b
Serg <your@serg.com>
2022-04-09 10:20:11 +0500

    Create MainViewModel

---
+++ Rocket-iOS/Rocket-iOS/Views/FirstScreenRocket/Model/MainViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  MainViewModel.swift
+//  Rocket-iOS
+//
+//  Created by Sergey Bodnar on 09.04.2022.
+//
+
+import Foundation
+import UIKit
+
+struct ImageRocket {
+    var imageRocket: UIImage?
+}
+
+struct NameSettingRocket {
+    var name: String?
+}
+
+struct InfoRocket {
+    var nameValue: String?
+    var value: String?
+}
+
+struct InfoRocketCollection {
+    var infoRocketCollection: [InfoRocket] = []
+}
+
+struct DescriptionRocket {
+    var nameDescription: String?
+    var valueDescription: String?
+}
+
+struct DescriptionRocketTable {
+    var descriptionRocketTable: [DescriptionRocket] = []
+}
+
+
