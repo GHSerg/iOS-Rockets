bc4cce97e76dcff42d6eaee5600c665362cbb3c9
Serg <your@serg.com>
2022-04-11 14:10:27 +0500

    Create view containers

---
+++ Rocket-iOS/Rocket-iOS/Views/SecondScreenRocket/View/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  Rocket-iOS
+//
+//  Created by Sergey Bodnar on 11.04.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
